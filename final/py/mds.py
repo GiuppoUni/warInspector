970068d11f95989c328c5790cd472cfb6c3ec71b
giuppo <giuppo@foo.com>
2020-04-04 16:32:55 +0200

    Add MDS

---
+++ final/py/mds.py
@@ -0,0 +1,123 @@
+"""
+=========================
+Multi-dimensional scaling
+=========================
+
+dataset: city_name,ihabitants (in thousands)
+
+We use the dissimilarity functions
+
+        dissM[i][j]= abs(ihabitants[i]-ihabitants[j])/(ihabitants[i]+ihabitants[j])
+        dissM[i][j]= abs (ihabitants[i]-ihabitants[j])
+        dissM[i][j]=1-jaccard(name[i],name[j])
+        dissM[i][j]=1-sameStart(name[i],name[j])
+        
+        
+
+
+"""
+
+print(__doc__)
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn import manifold
+import math
+from numpy import dot
+from numpy.linalg import norm
+
+def jaccard(s1,s2):
+    u=set(s1.lower()).union(set(s2.lower()))
+    i=set(s1.lower()).intersection(set(s2.lower()))
+    return(len(i)/len(u))
+
+def samePosition(s1,s2):
+    tot=0
+    for i in range( min(len(s1),len(s2))):
+        if s1[i]==s2[i]:
+            tot+=1
+    return(tot/min(len(s1),len(s2)))
+
+def sameStart(s1,s2):
+    tot=0
+    i=0
+    while i <( min(len(s1),len(s2))) and s1[i]==s2[i]:
+            i+=1
+    return(i/min(len(s1),len(s2)))
+
+data = pd.io.parsers.read_csv(
+     '../data/importNumbers2000-2018.csv' 
+    )
+print(data.columns, data)
+d=data["2019"] 
+d=d.fillna(0)
+
+print( "\n\n",np.all(np.isfinite(d)), not np.any(np.isnan(d)))
+
+countries=data["Country"]
+
+print(d,type(d),countries)
+
+dissM1=np.zeros((len(data),len(data))) #creates a zeros dissM
+dissM2=np.zeros((len(data),len(data))) #creates a zeros dissM
+dissM3=np.zeros((len(data),len(data))) #creates a zeros dissM
+
+for i in range(len(d)):
+    for j in range (len(d)):
+        dissM1[i][j]= abs(d[i]-d[j])
+        d_i=d[i] 
+        d_j=d[j]
+        if(d[i]+d[j]!= 0):
+            dissM2[i][j]= abs (d[i]-d[j])/(d[i]+d[j])
+        else:
+            dissM2[i][j]= abs (d[i]-d[j])
+
+        if( norm(d[i])*norm(d[j]) !=0):
+            dissM3[i][j]=cos_sim = dot(d[i], d[j])/(norm(d[i])*norm(d[j]))
+        else:
+            dissM3[i][j]=cos_sim = dot(d[i], d[j])
+
+        
+
+
+mds = manifold.MDS(n_components=2, max_iter=300, eps=1e-9,
+                   dissimilarity="precomputed")
+pos1 = mds.fit(dissM1).embedding_
+pos2 = mds.fit(dissM2).embedding_
+pos3 = mds.fit(dissM3).embedding_
+s = 50
+
+plt.scatter(pos1[:, 0], pos1[:, 1], color='red',s=s, lw=0, label='d[i]-d[j]')
+for label, x, y in zip(countries, pos1[:, 0], pos1[:, 1]):
+    plt.annotate(
+        label,
+        xy = (x, y), xytext = (-20, 20),
+        textcoords = 'offset points', ha = 'right', va = 'bottom',
+        bbox = dict(boxstyle = 'round,pad=0.3', fc = 'yellow', alpha = 0.5),
+        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+plt.legend()   
+plt.show()
+
+
+plt.scatter(pos2[:, 0], pos2[:, 1], color='red',s=s, lw=0, label='d[i]-d[j]/(d[i]+d[j])')
+for label, x, y in zip(countries, pos2[:, 0], pos2[:, 1]):
+    plt.annotate(
+        label,
+        xy = (x, y), xytext = (-20, 20),
+        textcoords = 'offset points', ha = 'right', va = 'bottom',
+        bbox = dict(boxstyle = 'round,pad=0.3', fc = 'yellow', alpha = 0.5),
+        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+plt.legend()   
+plt.show()
+
+plt.scatter(pos3[:, 0], pos3[:, 1], color='red',s=s, lw=0, label='abs(math.log10(d[i])-math.log10(d[j])')
+for label, x, y in zip(countries, pos3[:, 0], pos3[:, 1]):
+   plt.annotate(
+       label,
+       xy = (x, y), xytext = (20, 20),
+       textcoords = 'offset points', ha = 'right', va = 'bottom',
+       bbox = dict(boxstyle = 'round,pad=0.3', fc = 'yellow', alpha = 0.5),
+       arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+plt.legend()   
+plt.show()
+
--- final/py/pca.py
+++ final/py/pca.py
@@ -1,69 +1,182 @@
+
+
+"""
+=========================
+Multi-dimensional scaling
+=========================
+"""
+print(__doc__)
+
+
 from sklearn.decomposition import PCA
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import manifold
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from sklearn.utils.validation import check_symmetric
+import random
 
+DEBUG = True
+#Ramdom state
+RS = random.randint(0,1000000)
 
-
-
-""" Make avg of year intervals if present e.g 2000-2010 => 2005 """
+# Make avg of year intervals if present e.g 2000-2010 => 2005 
 def averize(str_interval):
     if("-" in str(str_interval)):
         splitted=str_interval.split("-")
         return int( int(splitted[0])+int(splitted[1])/2 )
     return str_interval
 
-#def main():
-# load dataset into Pandas DataFrame
-df = pd.read_csv("../data/merged.csv")
+def prepare_csv():
+
+    df = pd.read_csv("../data/merged.csv")
+    
+    if(DEBUG):    df = df.iloc[0:20]
+
+    print("Columns", df.columns)
+
+    df['Ordered'] = df['Ordered'].str.replace('(','')
+    df['Ordered'] = df['Ordered'].str.replace(')','')
+    df['Ordered'] = df['Ordered'].str.replace('.','')
+    df['Ordered'] = df["Ordered"].str.strip()
+    df['Ordered'] = pd.to_numeric(df['Ordered'])
+
+    df['Ordered year'] = df['Ordered year'].str.replace('(','')
+    df['Ordered year'] = df['Ordered year'].str.replace(')','')
+
+    df['Ordered year'] = pd.to_numeric(df['Ordered year'])
+
+    # df = df.drops("Delivered year")q
+    df["Delivered year"] =  df["Delivered year"].apply( averize )
+
+    df['Delivered num.'] = df['Delivered num.'].str.replace('(','')
+    df['Delivered num.'] = df['Delivered num.'].str.replace(')','')
+    df['Delivered num.'] = pd.to_numeric(df['Delivered num.'])
+    df["Delivered num."] = df["Delivered num."].fillna(0)
+    df['Delivered num.'] = df['Delivered num.'].astype(int)
+
+    print( df.groupby(["Supplier","Recipient"])['Delivered num.'].agg('sum').unstack(fill_value=0) )
+
+    recs= set( df["Recipient"].unique() )
+  
+    #Adding missing recipients in supplier column
+    for r in recs:
+        if r not in df["Supplier"]:
+            df=df.append({"Supplier":r}, ignore_index=True)
 
 
-from sklearn.preprocessing import StandardScaler
+    sups= set( df["Supplier"].unique() )
+    print("Sups",sups)
+    print("Recs",recs)
 
-print(df.columns)
-print(df.dtypes)
-# features = ['Supplier', 'Recipient', 'Ordered', 'Weapon model',
-#     'Weapon category', 'Ordered year', 'Delivered year',
-#     'Delivered num.', 'Comments', 'latS', 'longS', 'codeS', 'latR', 'longR',
-#     'codeR']
+    #print( set( df.loc[df["Supplier"]=="Australia"]["Recipient"] ) )
+    
+    for current in sups:
+        for missing in sups:
+            if( missing not in set( df.loc[df["Supplier"]==current]["Recipient"] ) ):
+                df  = df.append([{"Supplier":current,"Recipient":missing}], ignore_index=True)
+    #Adding missing suppliers in recipient column
+    # for s in sups:
+    #     if s not in df.loc[ df["Supplier"] == x ]["Recipient"]:
+    #         #Recipient not present in Supplier column (0 values)
+    #         for x in sups:
+    #             df.loc[ df["Supplier"] == x ]["Recipient"] = r
 
-df['Ordered'] = df['Ordered'].str.replace('(','')
-df['Ordered'] = df['Ordered'].str.replace(')','')
-df['Ordered'] = pd.to_numeric(df['Ordered'])
+    #states = np.concatenate(df["Supplier"].unique(),df["Recipient"].unique())
+    df["Delivered num."] = df["Delivered num."].fillna(0)
+    
+    print(df)
+    df = df.groupby(["Supplier","Recipient"])['Delivered num.'].agg('sum').unstack(fill_value=0) 
+    print( df )
 
-df['Ordered year'] = df['Ordered year'].str.replace('(','')
-df['Ordered year'] = df['Ordered year'].str.replace(')','')
-df['Ordered year'] = pd.to_numeric(df['Ordered year'])
-
-
-df['Delivered num.'] = df['Delivered num.'].str.replace('(','')
-df['Delivered num.'] = df['Delivered num.'].str.replace(')','')
-df['Delivered num.'] = pd.to_numeric(df['Delivered num.'])
-
-# df = df.drops("Delivered year")
-df["Delivered year"] =  df["Delivered year"].apply( averize )
-
-
-dfg=df.groupby(["Supplier","Recipient"])[["Delivered num."]].sum().astype(int)
-
-print(dfg,type(dfg))
-dfg = dfg.reset_index()
+    df.to_csv('pca_counts_min.csv', index=True) 
 
 
-# # Separating out the features
-x = dfg.loc[:, features].values
-# # Separating out the target
-y = dfg.loc[:,['Supplier','Recipient']].values
+def my_mds_plot():
+    
+    data = pd.io.parsers.read_csv(  #pandas handles in a better way
+     'pca_counts.csv', 
+     header="infer"         #the first row contains the recipiennts names
+    )
+    if(DEBUG):
+        data = pd.io.parsers.read_csv(  #pandas handles in a better way
+        'pca_counts_min.csv', 
+        header="infer"         #the first row contains the recipiennts names
+        ) 
+        print("Data\n",  data  )
 
+    dissM=data.values[:,1:] #the first column contains the suppliers names
+    countries=data.values[:,0]
+
+    #amax = np.amax(dissM)
+    #dissM /= amax          #you can uncomment  these lines to plot MDS in a normalized 2D
+
+    
+    dissM = np.array(dissM, dtype=float)
+    #Repair dissM which is not symmetric
+    dissM = check_symmetric(dissM)  
+    
+    if(DEBUG): 
+        print(dissM)
+        with open("log.txt","w") as out:
+            out.write("".join( [x for x in countries] ))
+            out.write("\n".join( [str(x) for x in dissM] ))
+        
+
+
+    mds = manifold.MDS(n_components=2, n_init=20,metric=True ,max_iter=3000, eps=1e-9,dissimilarity="precomputed",random_state=RS)
+    pos = mds.fit(dissM).embedding_
+    stress=mds.fit(dissM).stress_
+
+    plt.scatter(pos[:, 0], pos[:, 1], marker = 'o')
+    for label, x, y in zip(countries, pos[:, 0], pos[:, 1]):
+        plt.annotate(
+            label,
+            xy = (x, y), xytext = (-20, 20),
+            textcoords = 'offset points', ha = 'right', va = 'bottom',
+            bbox = dict(boxstyle = 'round,pad=0.3', fc = 'yellow', alpha = 0.1),
+            arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+    plt.show()
+    print(stress)
+
+
+
+    # from sklearn import datasets
+    # digits = datasets.load_digits()
+    # print(digits)
+
+    # tsne = manifold.TSNE(n_components=2,random_state=2).fit_transform(dissM)
+    # plt.scatter(tsne,tsne,3, marker = 'o')
+    # for label, x, y in zip(countries, pos[:, 0], pos[:, 1]):
+    #     plt.annotate(
+    #         label,
+    #         xy = (x, y), xytext = (-20, 20),
+    #         textcoords = 'offset points', ha = 'right', va = 'bottom',
+    #         bbox = dict(boxstyle = 'round,pad=0.3', fc = 'yellow', alpha = 0.1),
+    #         arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+    plt.show()
 
 
 # # Standardizing the features
 
-x = StandardScaler().fit_transform(x)
+def main():
+    # if(DEBUG): prepare_csv() # VERY SLOW
+    my_mds_plot()
+    # load dataset into Pandas DataFrame
+
+    # # Separating out the target
+    # y = df.loc[:,['Weapon description']].values
+    # # Standardizing the features
+    # x = StandardScaler().fit_transform(x)
+
+    # pca = PCA(n_components=2)
+    # principalComponents = pca.fit_transform(x)
+    # principalDf = pd.DataFrame(data = principalComponents
+    #              , columns = ['principal component 1', 'principal component 2'])
+
+main()
 
 
-pca = PCA(n_components=2)
-principalComponents = pca.fit_transform(x)
-principalDf = pd.DataFrame(data = principalComponents
-             , columns = ['principal component 1', 'principal component 2'])
-
-
-#main()
